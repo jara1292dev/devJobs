3bfe9fb1c7c62c3f2d34454c318b336e117e6602
Jose Rodríguez <antonio.rodriguez@fonix.com.mx>
2019-07-29 12:49:41 -0500

    Hanilitando Handlebars como template engine

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
 const router = require('./routes');
 
 const app = express();
+
+//habilitar handlebars como template engine
+app.engine('handlebars',
+    exphbs({
+        defaultLayout: 'layout'
+    })
+);
+app.set('view engine', 'handlebars');
+
+//static files
+app.set(express.static(path.join(__dirname, 'public')));
 
 app.use('/', router())
 app.listen(5000);
