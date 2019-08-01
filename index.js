8f87c243f7859aaa486fe7c22aef4aea4c55525b
jose <jara_1292@hotmail.com>
2019-08-01 14:24:54 -0500

    agregando un helper de handlebars al listado de skills

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ const app = express();
 //habilitar handlebars como template engine
 app.engine('handlebars',
     exphbs({
-        defaultLayout: 'layout'
+        defaultLayout: 'layout',
+        helpers: require('./helpers/handlebars')
     })
 );
 app.set('view engine', 'handlebars');
